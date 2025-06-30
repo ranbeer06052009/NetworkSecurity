97156b5ad7123066723cd2106e59898faa65c71e
ranbeer06052009 <tridev06052009@gmail.com>
2025-06-30 14:56:25 +0530

    Model Training and Mlflow connection completed

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from networksecurity.entity.config_entity import (
     DataIngestionConfig, TrainingPipelineConfig, 
     DataValidationConfig, DataTransformationConfig
 )
+
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import ModelTrainerConfig
 import sys
 
 if __name__ == '__main__':
@@ -36,5 +39,11 @@ if __name__ == '__main__':
         logging.info("Data Transformation Completed")
         print(data_transformation_artifact)
 
+        logging.info("Model Training stared")
+        model_trainer_config=ModelTrainerConfig(training_pipeline_config)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
     except Exception as e:
         raise NetworkSecurityException(e, sys)
